e898d047d222ccee0d983bdcec26f80fdc1a13db
honey bun <105636704+toaigel@users.noreply.github.com>
2022-06-12 00:33:24 +0300

    added sorting task

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-// Алфавит
+//  ------------ decoding task ------------
+
+// alphabet
 let symbols = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ', '.', ',', '—', '!'];
 
-// Закодированное сообщение
+// Encoded message
 let encodedSymbols = [18, 38, 46, 62, 66, 50, 33, 41, 66, 49, 48, 38, 58, 62, 68, 66, 48, 37, 42, 47, 66, 50, 33, 41, 66, 49, 48, 51, 49, 42, 67];
 
-// Раскодированное сообщение
+// Decoded message
 let decodedMessage = '';
 
 for (let element = 0; element < encodedSymbols.length; element++) {
   let elementNumber = encodedSymbols[element];
   let newLetter = symbols[elementNumber];
   decodedMessage += newLetter;
+}
+
+//  ------------ sorting task ------------
+
+let usersByDay = [4, 2, 1, 3];
+console.log(usersByDay);
+
+for (let i = 0; i <= usersByDay.length - 2; i++) {
+  let minValue = usersByDay[i];
+
+  for (let j = i + 1; j <= usersByDay.length - 1; j++) {
+    if (usersByDay[j] < minValue) {
+      minValue = usersByDay[j];
+      let swap = usersByDay[i];
+      usersByDay[i] = minValue;
+      usersByDay[j] = swap;
+    }
+  }
 }
